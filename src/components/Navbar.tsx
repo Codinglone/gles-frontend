07793ce3a9134d61c33488b55b9cf3b4907d93b2
codinglone <codinglone@gmail.com>
2023-05-29 17:31:56 +0400

    Modified navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,56 @@
-import { useState } from "react"
-import { Button } from "flowbite-react"
-
-const Navbar = () => {
+import { useState } from "react";
+import { Button, Navbar, Dropdown } from "flowbite-react";
+import { IoSchool } from "react-icons/io5";
+import { BsArrowRight } from "react-icons/bs"
+const Nav = () => {
   return (
-    <nav>
-        <Button>Click Me!</Button>
-    </nav>
-  )
-}
+    <>
+      <Navbar fluid rounded>
+        <Navbar.Brand className="text-blue-700" href="#">
+            <IoSchool className="mr-3 h-6 sm:h-9" />
 
-export default Navbar
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            E-APPLY
+          </span>
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+          <Button gradientDuoTone="purpleToBlue">Get Started <BsArrowRight className="ml-2 font-bold" /></Button>
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+          <Navbar.Link className="text-blue-700" href="/navbars">
+            Home
+          </Navbar.Link>
+          <Navbar.Link href="/navbars">About</Navbar.Link>
+          <Navbar.Link href="/navbars">Gallery</Navbar.Link>
+          <Navbar.Link href="/navbars">Contact</Navbar.Link>
+          <Dropdown
+      inline
+      label="Services"
+    >
+      <Dropdown.Header>
+        <span className="block text-sm">
+          E-Apply Services
+        </span>
+      </Dropdown.Header>
+      <Dropdown.Item>
+        Study Abroad
+      </Dropdown.Item>
+      <Dropdown.Item>
+        Register School
+      </Dropdown.Item>
+      <Dropdown.Item>
+        Study info
+      </Dropdown.Item>
+      <Dropdown.Divider />
+      <Dropdown.Item>
+        More info
+      </Dropdown.Item>
+    </Dropdown>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+};
+
+export default Nav;
