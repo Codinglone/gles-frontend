b684afcbb85a9349330259a6290f6301197c273d
codinglone <codinglone@gmail.com>
2023-05-29 16:22:50 +0400

    Modified website meta info

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <>
+    <Navbar />
      <main>
         <h1 className="text-teal-500">GLES Coming soon...</h1>
      </main>
