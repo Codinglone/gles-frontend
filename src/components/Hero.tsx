fbbe814d1ef9a8635e35f98831d04da71f78987c
codinglone <codinglone@gmail.com>
2023-05-31 15:58:52 +0400

    small naming changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,11 +9,11 @@ const Hero = () => {
             {/* <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"> checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p> */}
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Your gateway to study abroad excellence, apply to top universities with ease and unlock your global potential.</p>
             <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-blue-900 rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                Get started
+                More info
                 <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
             <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-blue-900  border border-blue-900 rounded-lg hover:bg-blue-900 focus:ring-4 focus:ring-blue-900 hover:text-white dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                Get intouch
+                Get started
             </a> 
         </div>
         <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
