70232951b57aba3f2dca19821be0b9f0f2c30bff
codinglone <codinglone@gmail.com>
2023-06-01 16:14:19 +0400

    Initialized tabs card

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@
 const Hero = () => {
   return (
     
-        <section className="bg-gray-white dark:bg-gray-900">
+        <section className="bg-gray-white dark:bg-gray-900 lg:min-h-[100vh] lg:mt-8 ">
     <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="text-blue-900 max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">Empowering Your Study Abroad Journey</h1>
--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -3,7 +3,13 @@ import { Tabs } from "flowbite-react"
 const Info = () => {
   return (
     <>
-      <section className="px-4 py-12">Tabs Section</section>
+      <section className="px-4 flex justify-center w-full">
+        <div className="flex justify-between shadow-lg my-2 lg:mx-4 lg:w-[50vw] w-full">
+          <span className="md:w-1/3 md:text-lg text-xs font-bold md:py-4 py-2 cursor-pointer text-blue-900 hover:text-blue-700 px-2">Why E-apply</span>
+          <span className="md:w-1/3 md:text-lg text-xs font-bold md:py-4 py-2 cursor-pointer text-blue-900 hover:text-blue-700 px-2">Admission Process</span>
+          <span className="md:w-1/3 md:text-lg text-xs font-bold md:py-4 py-2 cursor-pointer text-blue-900 hover:text-blue-700 px-2">Admission Duration</span>
+        </div>
+      </section>
     </>
   )
 }
