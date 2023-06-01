06e71559fdc56f04284674398a292eb2fcad1aaf
codinglone <codinglone@gmail.com>
2023-06-01 16:54:44 +0400

    Finished tabs functionality

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -11,13 +11,13 @@ const Info = () => {
     <>
       <section className="px-4 flex justify-center w-full">
         <div className="flex justify-between shadow-lg my-2 lg:mx-4 lg:w-[50vw] w-full">
-          <span className="md:w-1/3 md:text-lg text-xs font-bold md:py-4 py-2 cursor-pointer text-blue-900 hover:text-blue-700 px-2">
+          <span className="md:w-1/3 md:text-lg text-xs font-bold md:py-4 py-2 cursor-pointer text-blue-900 hover:text-blue-700 px-2" onClick={(): any => setTabData(whyData)}>
             Why E-apply
           </span>
-          <span className="md:w-1/3 md:text-lg text-xs font-bold md:py-4 py-2 cursor-pointer text-blue-900 hover:text-blue-700 px-2">
+          <span className="md:w-1/3 md:text-lg text-xs font-bold md:py-4 py-2 cursor-pointer text-blue-900 hover:text-blue-700 px-2" onClick={(): any => setTabData(processData)}>
             Admission Process
           </span>
-          <span className="md:w-1/3 md:text-lg text-xs font-bold md:py-4 py-2 cursor-pointer text-blue-900 hover:text-blue-700 px-2">
+          <span className="md:w-1/3 md:text-lg text-xs font-bold md:py-4 py-2 cursor-pointer text-blue-900 hover:text-blue-700 px-2" onClick={(): any => setTabData(durationData)}>
             Admission Duration
           </span>
         </div>
