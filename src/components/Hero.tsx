1d75351c32174a404a64687e1bc6aad461092562
codinglone <codinglone@gmail.com>
2023-06-01 17:04:02 +0400

    Added Link to the hero component that go to info section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 const Hero = () => {
   return (
@@ -11,9 +12,9 @@ const Hero = () => {
                 More info
                 <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
-            <a href="#" className="lg:mt-12 inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-blue-900  border border-blue-900 rounded-lg hover:bg-blue-900 focus:ring-4 focus:ring-blue-900 hover:text-white dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+            <Link to="/why" className="lg:mt-12 inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-blue-900  border border-blue-900 rounded-lg hover:bg-blue-900 focus:ring-4 focus:ring-blue-900 hover:text-white dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                 Get started
-            </a> 
+            </Link> 
         </div>
         <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
         <iframe src="https://embed.lottiefiles.com/animation/142376" className="w-full"></iframe>
